9748a64199a153c0bf03e888b5ef9c70ff8aa194
jana69305 <mjana7838@gmail.com>
2026-05-03 22:22:57 +0300

    Revert "settings controller"
    This reverts commit db7cdd16f3f6f4bc37fc2f59b27aa56b94f539e2.

--- src/com/controller/AuthController.java
+++ src/com/controller/AuthController.java
@@ -1,4 +1,5 @@
 package com.controller;
+
 import com.model.SecurityConfig;
 
 import java.io.*;
@@ -117,12 +118,5 @@ public void recordFailedAttempt() {
 // ── checks if user is currently locked out ────────────────────────────────
 public boolean isLockedOut() {
     return loadSecurity().isLockedOut();
-}
-
-
-public void togglePrivacyLock(boolean enabled) {
-    SecurityConfig sc = loadSecurity();
-    sc.setLockState(enabled);  
-    saveSecurity(sc);
 }
 }
